56e7e1b4b84750db0e678ea5805687b7da56718d
Chris Le <chrisl@seerinteractive.com>
2011-05-29 11:56:19 -0400

    Added a settings file for testing.

--- test/test_engine.rb
+++ test/test_engine.rb
@@ -1,22 +1,15 @@
 require File.expand_path('../helper', __FILE__)
+require File.expand_path('../settings', __FILE__)
 
 class TestEngine < Test::Unit::TestCase
 
-  DEFAULT_AUTH = {
-      :email => 'test@gmail.com',
-      :password => 'password',
-      :debug => false
-  }
-  DEFAULT_QUERY = {
-      :start_date => '2010-01-01',
-      :end_date => '2011-01-01',
-      :dimensions => ['date'],
-      :metrics => ['visits']
-  }
-  PROFILE_ID = 12345678
-
   def test_login_with_good_password
     assert Gattica.new(@auth), "should have been able to login"
+  end
+
+  def test_low_timeout
+    ga = Gattica.new(GatticaTest::DEFAULT_AUTH.merge!({ :debug =>false, :timeout => 1 }))
+    puts ga.accounts.inspect
   end
 
   def test_login_with_bad_user_password
@@ -36,28 +29,4 @@ class TestEngine < Test::Unit::TestCase
     assert Gattica.new({ :token => token })
   end
 
-  def test_max_results
-    response = ga_get({ :max_results => 5 })
-    assert response.points.count == 5, "should only return 5 results"
-  end
-
-  def test_start_index
-    response = ga_get({ :start_index => 5, :max_results => 5 })
-    assert response.points.first.title == "ga:date=20100105", "should start on the 5th"
-  end
-
-private
-
-  def ga(options={}, profile_id=PROFILE_ID)
-    unless defined? @ga
-      @ga = Gattica.new(DEFAULT_AUTH)
-      @ga.profile_id = profile_id
-    end
-    @ga
-  end
-
-  def ga_get(options={}, profile_id=PROFILE_ID)
-    ga.get(DEFAULT_QUERY.merge(options))
-  end
-  
 end

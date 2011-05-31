8fd20731dbca74aefe2e40af0311108ec01835a5
Chris Le <chrisl@seerinteractive.com>
2011-05-31 15:38:52 -0400

    bump version number.

--- lib/gattica.rb
+++ lib/gattica.rb
@@ -25,7 +25,7 @@ require 'gattica/segment'
 # Please see the README for usage docs.
 module Gattica
   
-  VERSION = '0.4.4'
+  VERSION = '0.4.7'
   
   # Creates a new instance of Gattica::Engine
   def self.new(*args)
